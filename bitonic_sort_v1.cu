c00ac6a3da1651d85c2350d63fa9f51421eb7225
NontasBak <nontasbak@gmail.com>
2025-01-13 13:37:08 +0200

    Finalize V1 implementation: If the distances are 512 or lower perform exchanges with 1 kernel call

--- bitonic_sort_v1.cu
+++ bitonic_sort_v1.cu
@@ -25,7 +25,7 @@ __global__ void exchange(int *arr, int size, int distance, int group_size) {
     }
 }
 
-__global__ void exchangeLocally(int *arr, int size) {
+__global__ void initialExchangeLocally(int *arr, int size) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     bool sort_descending = blockIdx.x & 1;
 
@@ -51,8 +51,25 @@ __global__ void exchangeLocally(int *arr, int size) {
     }
 }
 
+__global__ void exchangeLocally(int *arr, int group_size) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for (int distance = 1024 >> 1; distance > 0; distance >>= 1) {
+        int partner = tid ^ distance;
+        if (partner > tid) {
+            if ((tid & group_size) == 0 && arr[tid] > arr[partner]) {
+                swap(arr, tid, partner);
+            }
+            if ((tid & group_size) != 0 && arr[tid] < arr[partner]) {
+                swap(arr, tid, partner);
+            }
+        }
+        __syncthreads();
+    }
+}
+
 int main() {
-    int n = 1 << 12;
+    int n = 1 << 13;
     int num_threads = 1 << 10;
     int num_blocks = n / num_threads;
 
@@ -67,19 +84,24 @@ int main() {
     cudaMalloc(&d_arr, n * sizeof(int));
     cudaMemcpy(d_arr, arr, n * sizeof(int), cudaMemcpyHostToDevice);
 
-    exchangeLocally<<<num_blocks, num_threads>>>(d_arr, 1 << 10);
+    initialExchangeLocally<<<num_blocks, num_threads>>>(d_arr, 1 << 10);
 
     for (int group_size = 2048; group_size <= n; group_size <<= 1) {
-        for (int distance = group_size >> 1; distance > 0; distance >>= 1) {
+        for (int distance = group_size >> 1; distance > 512; distance >>= 1) {
             exchange<<<num_blocks, num_threads>>>(d_arr, n, distance,
                                                   group_size);
         }
+        exchangeLocally<<<num_blocks, num_threads>>>(d_arr, group_size);
     }
 
     cudaMemcpy(out, d_arr, n * sizeof(int), cudaMemcpyDeviceToHost);
 
+    // for (int i = 0; i < n; i++) {
+    //     if (i % 1024 == 0) printf("\n\n");
+    //     printf("%d ", out[i]);
+    // }
+
     for (int i = 1; i < n; i++) {
-        // printf("%d\n", out[i]);
         assert(out[i - 1] <= out[i]);
     }
 
